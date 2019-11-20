3d0db345579130d74922efd05bc9bdb9e7613c9e
Yogasmara <septianayoga30@gmail.com>
2019-11-20 23:24:28 +0700

    Adding expense Function

--- js/app.js
+++ js/app.js
@@ -39,11 +39,80 @@ class UI {
     const expense = this.totalExpense();
     const total = parseInt(this.budgetAmount.textContent) - expense;
     this.balanceAmount.textContent = total;
-  };
+    if (total < 0) {
+      this.balance.classList.remove('showGreen', 'showBlack');
+      this.balance.classList.add('showRed');
+    } else if (total > 0) {
+      this.balance.classList.remove('showRed', 'showBlack');
+      this.balance.classList.add('showGreen');
+    } else if (total === 0) {
+      this.balance.classList.remove('showRed', 'showGreen');
+      this.balance.classList.add('showBlack');
+    }
+  }
+
+  // Submit expense Form
+  submitExpenseForm() {
+    console.log("Testing my system");
+    const expenseValue = this.expenseInput.value;
+    const amountValue = this.amountInput.value;
+    if (expenseValue === '' || amountValue === '' || amountValue < 0) {
+      this.expenseFeedback.classList.add('showItem');
+      this.expenseFeedback.innerHTML = `<p>Cannot be empty or negative</p>`;
+      const self = this;
+      setTimeout(function () {
+        self.expenseFeedback.classList.remove("showItem");
+      }, 3000);
+    } else {
+      let amount = parseInt(amountValue);
+      this.expenseInput.value = "";
+      this.amountInput.value = "";
+
+      let expense = {
+        id: this.itemID,
+        title: expenseValue,
+        amount: amount
+      }
+      this.itemID++;
+      this.itemList.push(expense);
+      this.addExpense(expense);
+      this.showBalance();
+    }
+  }
+
+  // add expense
+  addExpense(expense) {
+    const div = document.createElement('div');
+    div.classList.add('expense');
+    div.innerHTML = `
+      <div class="expense-item d-flex justify-content-between align-items-baseline">
+
+      <h6 class="expense-title mb-0 text-uppercase list-item">${expense.title}</h6>
+      <h5 class="expense-amount mb-0 list-item">${expense.amount}</h5>
+
+      <div class="expense-icons list-item">
+
+        <a href="#" class="edit-icon mx-2" data-id="${expense.id}">
+        <i class="fas fa-edit"></i>
+        </a>
+        <a href="#" class="delete-icon" data-id="${expense.id}">
+        <i class="fas fa-trash"></i>
+        </a>
+      </div>
+      </div>
+    `;
+
+    this.expenseList.appendChild(div);
+  }
 
   // Total exprense method
   totalExpense() {
-    let total = 400;
+    let total = 0;
+
+
+
+    this.expenseAmount.textContent = total;
+
     return total;
   }
 
@@ -51,7 +120,7 @@ class UI {
 
 function eventListener() {
   const budgetForm = document.getElementById("budget-form");
-  const expenseForm = document.getElementById('budget-form');
+  const expenseForm = document.getElementById("expense-form");
   const expenseList = document.getElementById('expense-list');
 
   // New instance of the UI class
@@ -64,8 +133,9 @@ function eventListener() {
   });
 
   // exprense form submit
-  expenseForm.addEventListener('submit', function (event) {
+  expenseForm.addEventListener("submit", function (event) {
     event.preventDefault();
+    ui.submitExpenseForm();
   });
 
   // Budget click
